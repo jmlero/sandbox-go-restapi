3c59e469bbfb4ffd86bedee096da249091c5dc49
Jose Molero <jose.molero.ext@riag-digital.com>
2024-04-18 17:44:04 +0200

    add initial rest api

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+var (
+	productStore = make(map[string]Product)
+)
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+
+	productName := r.URL.Query().Get("name")
+	if product, ok := productStore[productName]; ok {
+		json.NewEncoder(w).Encode(product)
+	} else {
+		http.Error(w, "Product not found", http.StatusNotFound)
+	}
+}
+
+func createOrUpdateProduct(w http.ResponseWriter, r *http.Request) {
+
+	var prod Product
+	if err := json.NewDecoder(r.Body).Decode(&prod); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	productStore[prod.Name] = prod
+	json.NewEncoder(w).Encode(prod)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/product", getProduct)
+	http.HandleFunc("/updateProduct", createOrUpdateProduct)
+
+	http.ListenAndServe(":8080", nil)
 }
---
+++ model.go
@@ -0,0 +1,6 @@
+package main
+
+type Product struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
